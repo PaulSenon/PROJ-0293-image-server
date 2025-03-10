c78e31c8e7cfc35f90e35e3aedc069711c8f2feb
PaulSenon <paul.senon@gmail.com>
2025-03-10 16:02:58 +0900

    WIP but working in lambda local youhou

---
+++ app/esbuild.lambda.js
@@ -0,0 +1,19 @@
+import { build } from 'esbuild';
+
+// Run the build
+build({
+  entryPoints: ['src/lambdaEntrypoint.ts'],
+  bundle: true,
+  platform: 'node',
+  target: 'node22',
+  format: 'cjs',
+  outfile: 'dist/lambda/index.js',
+  external: ['aws-sdk', '@aws-sdk/*', 'sharp'],
+  sourcemap: true,
+  sourcesContent: false,
+})
+  .then(() => console.log('⚡ Lambda bundle built successfully!'))
+  .catch((error) => {
+    console.error('❌ Error building Lambda bundle:', error);
+    process.exit(1);
+  }); 
